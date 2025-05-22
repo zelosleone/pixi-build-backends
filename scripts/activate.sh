99f1d3b93b83831162039566b4cb95a26fc54636
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2025-05-22 15:06:06 +0200

    fix: unset linker for osx-64 and build backends with `rattler-build` (#163)

---
+++ scripts/activate.sh
@@ -0,0 +1,3 @@
+export CARGO_TARGET_DIR="target-pixi"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CONDA_PREFIX/bin/mold"
--- scripts/default_linker_osx_64.sh
+++
@@ -1,3 +0,0 @@
-# Ignore requiring a shebang as this is a script meant to be sourced
-# shellcheck disable=SC2148
-unset CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER
